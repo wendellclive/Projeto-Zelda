fb4066cce7e17f42d0d13049d0aad981486b84ef
Wendell Clive Santos de Lira <wendell@codata.pb.gov.br>
2024-02-12 20:18:48 -0300

    Desenvolvendo movimentos do player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [Header("Config Player")]
+    public float movementSpeed = 3f;
+
+    private Vector3 direction;
+
+    private CharacterController controller;
+    private Animator animator;
+    private bool isWalk;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        animator   = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        
+        direction = new Vector3(horizontal, 0f, vertical).normalized;
+        
+        if(direction.magnitude > 0.1f) 
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0, targetAngle, 0);
+            isWalk = true;
+        } else          // if(direction.magnitude <= 0.1f) 
+        {
+            isWalk = false;
+        }
+
+        controller.Move(direction * movementSpeed * Time.deltaTime);
+        animator.SetBool("isWalk", isWalk);
+
+    }
+}
